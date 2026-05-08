39c6fdb51e871984e0af15296b1e9a3ecd91b7e3
o-kazuo <itpro.kazu@gmail.com>
2026-05-09 04:41:43 +0900

    fix: ECSタスクロールにRekognition DetectLabels権限を追加

--- terraform/modules/container/main.tf
+++ terraform/modules/container/main.tf
@@ -290,3 +290,18 @@ resource "aws_iam_role_policy" "ecs_task_s3" {
   })
 }
 
+resource "aws_iam_role_policy" "ecs_task_rekognition" {
+  name = "${var.env}-ecs-task-rekognition-policy"
+  role = aws_iam_role.ecs_task.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["rekognition:DetectLabels"]
+        Resource = "*"
+      }
+    ]
+  })
+}
